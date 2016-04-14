ab76d45244db7ec0992b220b862f7daf0adc60b3
fermat <fermat1217@gmail.com>
2016-04-14 16:47:06 +0100

    hacking the parser

--- Parser.hs
+++ Parser.hs
@@ -1,14 +1,18 @@
+{-#LANGUAGE PackageImports, StandaloneDeriving, DeriveDataTypeable, FlexibleContexts#-}
 module Parser where
+       
+import Syntax
+
 import Text.Parsec hiding (ParseError,Empty, State)
 import qualified Text.Parsec as P
 import Text.Parsec.Language
 import Text.Parsec.Char
 import Text.Parsec.Expr(Operator(..),Assoc(..),buildExpressionParser)
 import qualified Text.Parsec.Token as Token
-import Text.Parsec.Indent
 
 import Control.Applicative hiding ((<|>),many, optional)
 import Control.Monad.State.Lazy
+import "mtl" Control.Monad.Identity
 import Control.Exception(Exception)
 
 import qualified Data.IntMap as IM
@@ -16,8 +20,184 @@ import Data.Typeable
 import Data.Char
 import Data.List
 
-parseTRS :: String -> String -> Either P.ParseError Module
-parseModule srcName cnts =
-  runIndent srcName $
-  runParserT gModule initialParserState srcName cnts
+-- parseModule :: String -> String -> Either P.ParseError Module
+-- parseModule srcName cnts =
+--   runIndent srcName $
+--   runParserT gModule initialParserState srcName cnts
+
+type Parser a = Parsec String () a
+
+
+binOp :: Assoc -> String -> (a -> a -> a) -> Operator String u Identity a
+binOp assoc op f = Infix (reservedOp op >> return f) assoc
+
+postOp :: String -> (a -> a) -> Operator String u Identity a
+postOp op f = Postfix (reservedOp op >> return f)
+
+preOp :: String -> (a -> a) -> Operator String u Identity a
+preOp op f = Prefix (reservedOp op >> return f)
+
+toOp op "infix" app var = binOp AssocNone op (binApp op app var)
+toOp op "infixr" app var = binOp AssocRight op (binApp op app var)
+toOp op "infixl" app var = binOp AssocLeft op (binApp op app var)
+toOp op "pre" app var = preOp op (preApp op app var)
+toOp op "post" app var = postOp op (postApp op app var) 
+toOp _ fx app var = error (fx ++ " is not a valid operator fixity.")
+
+postApp op app var x = app (var op) x
+
+preApp op app var x = app (var op) x
+
+binApp op app var x y = app (app (var op) x) y
+
+deriving instance Typeable P.ParseError
+instance Exception P.ParseError 
+
+-- parse module
+gModule :: Parser Module
+gModule = do
+  bs <- many gDecl
+  eof
+  return $ Module bs
+
+gDecl :: Parser Decl
+gDecl = ruleDecl
+
+ruleDecl :: Parser Decl
+ruleDecl = do
+  c <- con 
+  reservedOp ":"
+--  pos <- getPosition
+  t <- rule
+  return $ Rule c t
+  
+var :: Parser Exp
+var = do
+  n <- identifier
+  when (isUpper (head n)) $ parserZero
+  return (Var n)
+
+ensureLower :: Parser String
+ensureLower = do
+  n <- identifier
+  when (isUpper (head n)) $ unexpected "expected to begin with lowercase letter"
+  return n
+
+ensureUpper :: Parser String
+ensureUpper = do
+  n <- identifier
+  when (isLower (head n)) $ unexpected "expected to begin with uppercase letter"
+  return n
+
+con :: Parser Exp
+con = do
+  n <- identifier
+  when (isLower (head n)) $ parserZero
+  return (Constr n)
+
+-- parser for FType--
+rule :: Parser Exp
+rule = buildExpressionParser ruleOpTable term
+
+term :: Parser Exp
+term =  compound 
+
+ruleOpTable :: [[Operator String u Identity Exp]]
+ruleOpTable = [[binOp AssocNone "~>" Arrow]]
+
+compound = do
+  n <- try var <|> con
+  as <- compoundArgs
+  if null as then return n
+    else return $ foldl' (\ z x -> App z x) n as 
+
+compoundArgs = 
+  many (try con <|> try var <|> try (parens term))
+
+
+-----------------------Positions -------
+  
+wrapPos :: Parser Exp -> Parser Exp
+wrapPos p = pos <$> getPosition <*> p
+  where pos x (Pos y e) | x==y = (Pos y e)
+        pos x y = Pos x y
+
+
+-------------------------------
+
+-- Tokenizer definition
+
+gottlobStyle :: (Stream s m Char, Monad m) => Token.GenLanguageDef s u m
+gottlobStyle = Token.LanguageDef
+                { Token.commentStart   = "{-"
+                , Token.commentEnd     = "-}"
+                , Token.commentLine    = "--"
+                , Token.nestedComments = True
+                , Token.identStart     = letter
+                , Token.identLetter    = alphaNum <|> oneOf "_'"
+                , Token.opStart        = oneOf ":!#$%&*+.,/<=>?@\\^|-"
+                , Token.opLetter       = (oneOf ":!#$%&*+.,/<=>?@\\^|-") <|> alphaNum
+                , Token.caseSensitive  = True
+                , Token.reservedNames =
+                  [
+                    "forall", "iota", "reduce", 
+                    "cmp","invcmp", "inst", "mp", "discharge", "ug", "beta", "invbeta",
+                    "by", "from", "in", "let", "simpCmp", "invSimp",
+                    "case", "of",
+                    "data", "if", "then", "else",
+                    "axiom", "proof", "qed", "lemma", "auto",
+                    "show",
+                    "where", "module",
+                    "infix", "infixl", "infixr", "pre", "post",
+                    "formula", "prog", "set",
+                    "tactic", "deriving", "Ind"
+                  ]
+               , Token.reservedOpNames =
+                    ["\\", "->", "|", ".","=", "::", ":", "=>"]
+                }
+
+tokenizer :: Token.GenTokenParser String u Identity
+tokenizer = Token.makeTokenParser gottlobStyle
+
+identifier :: ParsecT String u Identity String
+identifier = Token.identifier tokenizer
+
+whiteSpace :: ParsecT String u Identity ()
+whiteSpace = Token.whiteSpace tokenizer
+
+reserved :: String -> ParsecT String u Identity ()
+reserved = Token.reserved tokenizer
+
+reservedOp :: String -> ParsecT String u Identity ()
+reservedOp = Token.reservedOp tokenizer
+
+operator :: ParsecT String u Identity String
+operator = Token.operator tokenizer
+
+colon :: ParsecT String u Identity String
+colon = Token.colon tokenizer
+
+integer :: ParsecT String u Identity Integer
+integer = Token.integer tokenizer
+
+brackets :: ParsecT String u Identity a -> ParsecT String u Identity a
+brackets = Token.brackets tokenizer
+
+parens :: ParsecT String u Identity a -> ParsecT String u Identity a
+parens  = Token.parens tokenizer
+
+braces :: ParsecT String u Identity a -> ParsecT String u Identity a
+braces = Token.braces tokenizer
+
+dot :: ParsecT String u Identity String
+dot = Token.dot tokenizer
+
+commaSep1 :: ParsecT String u Identity a -> ParsecT String u Identity [a]
+commaSep1 = Token.commaSep1 tokenizer
+
+semiSep1 :: ParsecT String u Identity a -> ParsecT String u Identity [a]
+semiSep1 = Token.semiSep1 tokenizer
+
+comma :: ParsecT String u Identity String
+comma = Token.comma tokenizer
 
---
+++ Syntax.hs
@@ -0,0 +1,180 @@
+module Syntax where
+
+import Control.Monad.State.Lazy
+import Control.Monad.Reader
+
+import Data.Char
+import qualified Data.Set as S
+import Data.List hiding (partition)
+
+import Text.Parsec.Pos
+
+type Name = String
+
+-- merge prog, kind, type into one syntactic category.
+data Exp = Var Name
+          | Const Name
+          | Constr Name
+          | App Exp Exp 
+          | Lambda Name Exp 
+          | Pos SourcePos Exp
+          | Imply Exp Exp
+          | Arrow Exp Exp
+          | Forall Name Exp
+          deriving (Show, Eq, Ord)
+data Module = Module [Decl] deriving (Show)
+
+data Decl = Rule Exp Exp
+          deriving Show
+
+                      
+-- freeVar :: Exp -> S.Set Name
+-- freeVar (Var x) = S.insert x S.empty
+-- freeVar (Const x) = S.empty
+-- freeVar (Constr x) = S.empty
+-- freeVar (Arrow f1 f2) = (freeVar f1) `S.union` (freeVar f2)
+-- freeVar (App f1 f2) = (freeVar f1) `S.union` (freeVar f2)
+-- freeVar (Forall x f) = S.delete x (freeVar f)
+-- freeVar (Imply bds h) = S.unions (map freeVar bds) `S.union` freeVar h
+
+-- freeKVar :: Exp -> S.Set Name
+-- freeKVar Star = S.empty
+-- freeKVar (KVar x) = S.insert x S.empty
+-- freeKVar (KArrow f1 f2) = (freeKVar f1) `S.union` (freeKVar f2)
+
+
+-- note that this is a naive version of getting
+-- free variable, since fv  will consider data
+-- type constructors and program definitions as
+-- free variables. So one would need to aware
+-- of this when using fv.
+-- type Subst = [(Name, Exp)]        
+
+-- fv :: Exp -> S.Set Name
+-- fv (Con x) = S.empty
+-- fv (EVar x) = S.insert x S.empty
+-- fv (App f1 f2) = fv f1 `S.union` fv f2
+-- fv (Lambda x s) = S.delete x (fv s)
+-- fv (Let bs p) =
+--   let binds = S.fromList $ map fst bs
+--       tvars = S.unions $ map fv $ map snd bs
+--       pvar = fv p in
+--   S.difference (S.union pvar tvars) binds
+
+-- fv (Match p cases) =
+--   S.unions (map fvCase cases) `S.union` fv p
+--     where fvCase (c, params, t) =
+--             S.difference (fv t) (S.fromList params)
+            
+-- fv (Pos _ t) = fv t
+
+-- -- applyQ currently only used at freshInst
+
+-- applyQ :: Subst -> QType -> QType
+-- applyQ subs (DArrow fs f) =
+--   let fs' = map (applyE subs) fs
+--       f' = applyE subs f in
+--   DArrow fs' f'
+      
+-- applyE :: Subst -> Exp -> Exp
+-- applyE subs a@(Con x) = a
+-- applyE subs (EVar x) =
+--   case lookup x subs of
+--     Just x1 -> x1
+--     Nothing -> EVar x
+
+-- applyE subs (Arrow f1 f2) =
+--   let a1 = applyE subs f1
+--       a2 = applyE subs f2 in
+--   Arrow a1 a2
+
+-- applyE subs (Forall y f) =
+--  let subs' = filter (\(x, _) -> not (x == y)) subs in
+--  Forall y (applyE subs' f)
+
+-- applyE subs (FApp f1 f2) =
+--   let a1 = applyE subs f1
+--       a2 = applyE subs f2 in
+--   FApp a1 a2
+
+-- applyE subs (Imply bs h) =
+--   let a1 = applyE subs h
+--       a2 = map (applyE subs) bs in
+--   Imply a2 a1
+        
+-- -- substituting term
+-- applyE subs (App a b) = App (applyE subs a) (applyE subs b)
+
+-- applyE subs (Lambda x p) = Lambda x (applyE subs p)
+
+-- applyE subs (Match p branches) =
+--   Match (applyE subs p) $ map (helper subs) branches
+--   where helper subs (c, xs, p) = (c, xs, applyE subs p)
+
+-- applyE subs (Let xs p) =
+--   Let (map (helper subs) xs) (applyE subs p)
+--   where helper subs (x, def) = (x, applyE subs def)
+
+-- applyE subs (Pos _ p) = applyE subs p
+
+-- firstline (Inst a xs) = Inst a [head xs]
+
+-- apply :: Exp -> Name -> Exp -> Exp
+-- apply a x (Con t) = Con t
+-- apply a x (EVar t) = if x == t then a else (EVar t)
+-- apply a x (App t1 t2) = App (apply a x t1) (apply a x t2)
+-- apply a x t1@(Lambda y t) =
+--   if x == y then t1
+--   else Lambda y (apply a x t)
+-- apply a x (Match t branches) = Match (apply a x t) $ map (helper a x) branches
+--  where helper b y (c, args, p) = if y `elem` args then (c, args, p) else (c, args, apply b y p)
+-- apply a x (Let branches p) =
+--   let binds = map fst branches in
+--   if x `elem` binds then (Let branches p)
+--   else Let (map (\ (e,d) -> (e, apply a x d)) branches) (apply a x p)
+-- apply a x t1@(Forall y t) =
+--   if x == y then t1
+--   else Forall y (apply a x t)
+
+-- apply a x (FApp p1 p2) = FApp (apply a x p1) (apply a x p2)
+-- apply a x (Imply bds h) = Imply (map (apply a x) bds) (apply a x h)
+-- apply a x (Pos _ p) = apply a x p       
+
+-- applyK :: [(Name, Exp)] -> Exp -> Exp
+-- applyK _ Star = Star
+
+-- applyK subs (KVar x) =
+--   case lookup x subs of
+--     Just x1 -> x1
+--     Nothing -> KVar x
+
+-- applyK subs (KArrow f1 f2) =
+--   let a1 = applyK subs f1
+--       a2 = applyK subs f2 in
+--   KArrow a1 a2
+
+-- flatApp (Pos _ p) = flatApp p
+-- flatApp (App t1 t2) = flatApp t1 ++ [t2]
+-- flatApp (FApp t1 t2) = flatApp t1 ++ [t2]
+-- flatApp t = [t]
+
+-- makeName name = do
+--   m <- get
+--   modify (+1)
+--   return $ name ++ show m
+
+-- inst (Forall x t) = do
+--    newVar <- makeName "x"
+--    t' <- inst t
+--    return $ apply (EVar newVar) x t'
+-- inst a = return a   
+
+-- quantify t =
+--   let fs = S.toList $ freeVar t in
+--   foldr (\ z x -> Forall z x) t fs
+  
+-- flatten :: Exp -> [Exp]
+-- flatten (Pos _ f) = flatten f
+-- flatten (Arrow f1 f2) = f1 : flatten f2
+-- flatten (KArrow f1 f2) = f1 : flatten f2
+-- flatten _ = []
