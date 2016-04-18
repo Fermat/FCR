9e1bc45a7bfb8f832cfb9199d2b1c40b61b25594
fermat <fermat1217@gmail.com>
2016-04-18 14:53:26 +0100

    add a file

---
+++ src/Cegt/Monad.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE NamedFieldPuns, DeriveDataTypeable, ParallelListComp, GeneralizedNewtypeDeriving, FlexibleInstances, FlexibleContexts  #-}
+module Cegt.Monad where
+import Cegt.Syntax
+import Cegt.PrettyPrinting
+
+import Text.PrettyPrint
+import Data.Typeable
+import Control.Monad.State
+import Control.Monad.Except
+import Control.Exception
+import qualified Data.Map as M
+import Control.Applicative hiding (empty)
+import Control.Monad.Reader
+import Text.Parsec.Pos
+
+
+data Env = Env{axioms :: [(Name, Exp)], lemmas :: [(Name, Exp)]}
+         deriving Show
+
+
+emptyEnv :: Env
+emptyEnv = Env {axioms = [], lemmas = []}
+                  
+extendAxiom :: Name -> Exp -> Env -> Env
+extendAxiom v ts e@(Env {axioms}) = e{axioms =  (v , ts) : axioms}
+
+extendLemma :: Name -> Exp -> Env -> Env
+extendLemma v t e@(Env {lemmas}) = e{lemmas = (v,  t):lemmas}
