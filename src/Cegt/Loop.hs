5a436de47f030c067ee28ce17abb05e7e224eb85
fermat <fermat1217@gmail.com>
2016-04-27 17:32:10 +0100

    constructing partial proof

--- src/Cegt/Loop.hs
+++ src/Cegt/Loop.hs
@@ -1,11 +1,10 @@
 module Cegt.Loop where
 import Cegt.Syntax
 import Cegt.Rewrite
-
 import Data.List
 
 getList :: Trace -> [Name]
-getList (Trace ls) = map (\(a,_,_)-> a) ls
+getList (Trace ls) = map (\(_, _, a,_)-> a) ls
 
 findCycle :: [Name] -> [Name] -> ([Name], [Name])
 findCycle [] pre = (pre, [])
@@ -13,8 +12,15 @@ findCycle (y:ys) pre = case findIndex (\x -> x == y) ys of
                           Nothing -> findCycle ys (pre++[y])
                           Just i ->  (pre, y : take i ys)
 
-cycleProof :: ([Name], [Name]) -> [(Name, Exp)] -> Exp -> [(Name, Exp)]
-cycleProof (pre, cyc) env e = resolve pre env e
+-- cycleProof :: ([Name], [Name]) -> [(Name, Exp)] -> Exp -> [(Name, Exp)]
+-- cycleProof (pre, cyc) env e = resolve pre env e
 
-resolve :: [Name] -> [(Name, Exp)] -> (Exp, Exp)
+construct ::  [(Pos, Subst, Name, Exp)] -> Exp
+construct [] = (Var "partial")
+construct ((p, s, n, e):xs) =
+  let codomain = map snd s
+      cxt = Lambda "hole" $ replace e p (Var "hole")
+      pf = foldl' (\ z x -> App z x) (App (Const n) cxt) codomain
+  in Lambda "partial" $ App pf (construct xs)
+
 
