c475250d6334177306e147d64e50a960522e2b4f
Fermat <fermat1217@gmail.com>
2016-05-16 11:58:51 +0000

    add a new tactic use

--- src/Cegt/Interaction.hs
+++ src/Cegt/Interaction.hs
@@ -20,8 +20,8 @@ normalize (Imply t t') = Imply (normalize t) (normalize t')
 normalize (Forall x t) = Forall x (normalize t)
 -- normalize a = error $ show a
 
-quantify :: Exp -> ([Name], Exp)
-quantify a@(Arrow t t') = ("p":(free a), Imply (App (Var "p") t') (App (Var "p") t))
+-- quantify :: Exp -> ([Name], Exp)
+-- quantify a@(Arrow t t') = ("p":(free a), Imply (App (Var "p") t') (App (Var "p") t))
 
 type PfEnv = [(Name, Exp)]
 type ProofState = (Name, Exp, [(Pos, Exp, PfEnv)])
@@ -61,8 +61,9 @@ apply (gn, pf, (pos, goal, gamma):res) k ins =
                   head'' = applyE renaming head
                   body' = map normalize $ (map (applyE sub) body'')
                   head' = normalize $ applyE sub head''
-              in if head' /= goal then error $ "error apply" ++ show head' ++ "--" ++ show goal
-                                       ++ show sub ++ "--" ++ show head -- Nothing
+              in if head' /= goal then Nothing
+                                       -- error $ "error apply" ++ show head' ++ "--" ++ show goal
+                                       -- ++ show sub ++ "--" ++ show head 
                  else let np = ins++body'
                           name = case k of
                                    n:_ -> if isUpper n then Const k else Var k
@@ -73,6 +74,29 @@ apply (gn, pf, (pos, goal, gamma):res) k ins =
                           ps = map (\ x -> pos++x++[1]) zeros
                           new = map (\(p, g) -> (p, g, gamma)) $ zip ps body'
                       in Just (gn, pf', new++res)  
+
+use :: ProofState -> Name -> [Exp] -> Maybe ProofState
+use (gn, pf, []) k ins = Just (gn, pf, [])
+use (gn, pf, (pos, goal, gamma):res) k ins = 
+  case lookup k gamma of
+    Nothing -> Nothing
+    Just f -> let (vars, bare) = getVars f
+                  fresh = map (\ (v, i) -> v ++ show i ++ "fresh") $ zip vars [1..]
+                  renaming = zip vars (map Var fresh)
+                  sub = zip fresh ins
+                  b'' = applyE renaming bare
+                  b' =  normalize $ applyE sub b''
+                  f' = foldl' (\t x -> Forall x t) b' (free b')
+              in if f' `alphaEq` goal then
+                   let name = case k of
+                                   n:_ -> if isUpper n then Const k else Var k
+                                   a -> error "unknow error from apply"
+                       contm = foldl' (\ z x -> App z x) name ins
+                       pf' = replace pf pos contm
+                   in Just (gn, pf', res)  
+                 else Nothing
+                   
+                   
 
 separate f = let (vars, imp) = getVars f
                  (bs, h) = getPre imp
@@ -91,49 +115,8 @@ makeZeros n | n > 0 = make n : makeZeros (n-1)
 stream = 0:stream
 make n | n > 0 = take (n-1) stream
 
-test1 = disp $ applyE [("x",App (Const "S") (Var "y")),("y",Const "Z")] (App (App (Var "d") (App (Const "S") (Var "x"))) (Var "y"))
-
--- test1 = apply gamma1 "h2" [Const "Z"]
--- gamma1 = ([("h1",Forall "x" (Forall "y" (Imply (App (App (Var "d") (Var "x")) (App (Const "S") (Var "y"))) (App (App (Var "d") (App (Const "S") (Var "x"))) (Var "y"))))),("h2",Forall "y" (Imply (App (App (Var "d") (App (Const "S") (Var "y"))) (Const "Z")) (App (App (Var "d") (Const "Z")) (Var "y"))))],Lambda "d" (Lambda "h1" (Lambda "h2" (App (App (Var "d") (Const "Z")) (Const "Z")))),[([1,1,1],App (App (Var "d") (Const "Z")) (Const "Z"))])
-
--- gamma2 = 
--- test2 = disp $ normalize $ applyE [("d", (Lambda "x" (Lambda "y" (App (App (Var "d") (Var "x")) (App (Const "S") (Var "y"))))))] (Imply (Forall "y" (Imply (App (App (Var "d") (App (Const "S") (Var "y"))) (Const "Z")) (App (App (Var "d") (Const "Z")) (Var "y")))) (App (App (Var "d") (Const "Z")) (Const "Z")))
-{-
-apply :: Exp -> Exp -> [(Name, Exp)] -> Name -> [Exp] -> Maybe Exp
-apply goal cxt env k ins = case lookup k env of
-                             Nothing -> Nothing
-                             Just (Arrow t t') -> let (xs, Imply e e') = quantify $ Arrow t t'
-                                                      sub = zip xs ins
-                                                      body = normalize $ applyE sub e
-                                                      head = normalize $ applyE sub e'
-                                                  in if head == goal then Just $ () body
-                                                     else Nothing
 
 
-
-
-
-resolve :: Exp -> [(Name, Exp)] -> Name -> Maybe Exp
-resolve goal env k = case lookup k env of
-                           Nothing -> Nothing
-                           Just (Arrow t t') ->
-                             let (vars, Imply e e') = quantify $ Arrow t t'
-                                 instCxt = [(p, s) | (p, e) <- getSubterms goal, s <- match t e, s /= []]
-                             in case instCxt of
-                               [] -> Nothing
-                               (p,ins):xs ->
-                                 let
-                                   cxt = ("p", Lambda "y" (replace e p (Var "y")))
-                                   sub = cxt:ins
-                                   sub' = helper sub vars
-                                   body = normalize $ applyE sub' e
-                                   head = normalize $ applyE sub' e'
-                                 in Just body
-                     where helper s [] = []
-                           helper s (y:ys) = case lookup y s of
-                                                   Nothing -> (y, Var y) : helper s ys
-                                                   Just t -> (y, t) :helper s ys
--}                                        
                                            
                      
                  
