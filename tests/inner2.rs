44fdec75a99af4992be0ca116818a6efe3555864
fermat <fermat1217@gmail.com>
2016-05-17 15:03:02 +0100

    minor

--- tests/inner2.rs
+++ tests/inner2.rs
@@ -4,7 +4,7 @@ K3 : G (H x y) ~> J y
 
 lemma h : forall f x z . (forall p x y . p (f (S x) (G (H x z))) => p (f x y)) => (forall p x y . p (H x (S y)) => p (H (S x) y)) => (forall p x y .p (J y) => p (G (H x y))) => f (S x) (G (H x z))
 proof
-coind h
+coind
 intros f x z a1 a2 a3
 apply a3 (\ y. f (S x) y) x z  -- f (S x) (J z)
 apply a1 (\ y . y) (S x) (J z) -- f (S (S x)) (G (H (S x) z))
