8fc19a02db69e461c2b06e93899f229baa54515d
fermat <fermat1217@gmail.com>
2016-06-02 23:03:41 +0100

    found a bug in the hmatch, and applyh when the quantified vars are more than the free vars in the body

---
+++ tests/fib.rs
@@ -0,0 +1,7 @@
+Ka : A x ~> A (B x)
+Kb : B x ~> A x
+
+g : forall a b x .
+      (forall p y . p (a y) => p (b y)) => 
+      (forall p y . p (a (b y)) => p (a y)) => b x
+g b a = b (g a (\ b1 . a (b b1)))
