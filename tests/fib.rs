4ee83163aea738c7a25a308983c8cb78a680729f
fermat <fermat1217@gmail.com>
2016-06-02 23:17:27 +0100

    the user has the ability to rearrange the order, so now existential var is easier to deal with

--- tests/fib.rs
+++ tests/fib.rs
@@ -5,3 +5,10 @@ g : forall a b x .
       (forall p y . p (a y) => p (b y)) => 
       (forall p y . p (a (b y)) => p (a y)) => b x
 g b a = b (g a (\ b1 . a (b b1)))
+{- 
+
+g : forall a b x .
+      (forall p y . p (a (b y)) => p (a y)) => 
+      (forall p y . p (a y) => p (b y)) => b x
+g a b = b (g (\ b1 . a (b b1)) a)
+-}
--- tests/inner2.rs
+++ tests/inner2.rs
@@ -1,13 +1,22 @@
 K1 : F x y ~> F (S x) (G (H x Z))
 K2 : H (S x) y ~> H x (S y)
 K3 : G (H x y) ~> J y
-
+{-
 g : forall p' f x z . (forall p x y . p (f (S x) (G (H x z))) => p (f x y)) => 
                           (forall p x y . p (H x (S y)) => p (H (S x) y)) => 
                               (forall p x y . p (J y) => p (G (H x y))) => 
                                    p' (f (S x) (G (H x z)))
 
 g a1 a2 a3 = a3 (a1 (a2 (g (\ b1 . a1 (a2 b1)) a2 a3)))
+-}
+
+g' : forall p' f x z . (forall p x y . p (J y) => p (G (H x y))) =>
+                           (forall p x y . p (f (S x) (G (H x z))) => p (f x y)) => 
+                               (forall p x y . p (H x (S y)) => p (H (S x) y)) => 
+                                   p' (f (S x) (G (H x z)))
+
+g' a3 a1 a2 = a3 (a1 (a2 (g' a3 (\ b1 . a1 (a2 b1)) a2)))
+
 -- why the hell does this work? 
 -- thankgoodness we have faithfulness
 -- g a1 a2 a3 = a1 (a1 (a1 (a2 (g (\ b1 . a1 (a2 b1)) a2 a3))))
@@ -15,7 +24,7 @@ g a1 a2 a3 = a3 (a1 (a2 (g (\ b1 . a1 (a2 b1)) a2 a3)))
 
 -- h : F (S Z) (G (H Z Z))
 h : F (S x) (G (H x Z))
-h = g K1 K2 K3
+h = g' K3 K1 K2 
 
 {-
 lemma h : forall p' f x z . (forall p x y . p (f (S x) (G (H x z))) => p (f x y)) => 
