4a3001f3c9034802f88be8e4d6585f167764b9dd
Fermat <fermat1217@gmail.com>
2016-06-03 18:50:19 +0000

    realize the old solution for this one is not god

--- tests/zantema-geser.rs
+++ tests/zantema-geser.rs
@@ -1,8 +1,16 @@
 K : F Z (S x) y ~> G (F Z x (S y)) (F x y (S (S Z)))
-
+-- G (F Z Z (S y)) (F Z y (S (S Z)))
+{-
 f : forall p qa qb f . (forall p x y . p (qa (f Z x (S y))) => p (f Z (S x) y)) => 
                        (forall p y . p (qb (f Z y (S (S Z)))) => p (f Z (S Z) y)) => p (f Z (S Z) (S Z)) 
 f a1 a2 = a2 (f a1 (\ c . (a2 (a1 c))))
+-}
 
-h : F Z (S Z) (S Z)
-h = f K K
+
+f : forall p f g . (forall p x y . p (g (f Z x (S y)) (f x y (S (S Z)))) => p (f Z (S x) y)) => 
+                       (forall p x y . p (g (f Z x (S y)) (f x y (S (S Z)))) => p (f Z (S x) y)) => p (f Z (S Z) (S Z)) 
+f a1 a2 = a2 (f a1 (\ c . (a2 (a1 c))))
+
+
+-- h : F Z (S Z) (S Z)
+-- h = f K K
