70d03c9c2e9656c23d353593d01f533f6a3cab88
Fermat <fermat1217@gmail.com>
2016-06-03 14:08:25 +0000

    a bug found in cxt free

--- tests/cxt-free.rs
+++ tests/cxt-free.rs
@@ -6,3 +6,15 @@ K5 : B (X x) ~> X (Cl (Y x))
 K6 : Bl (Cl (Dl x)) ~> X (Cl (Y x))
 K7 : Y (D x) ~> Dl (Y x)
 K8 : Y (El x) ~> Dl (Dl (El x))
+
+f : forall p0 c b d y . (forall p x . p (B (Bl x)) => p (Bl (B x))) =>
+    	      	      	(forall p x . p (B ( c (D x))) => p (Bl ( c (Dl x)))) =>
+			(forall p x . p (Dl (D x)) => p (D (Dl x))) =>
+			(forall p x . p (Al (Bl (Bl x))) => p (Al (X x))) =>
+			(forall p x . p (X (Cl (Y x))) => p (B (X x))) =>
+			(forall p x . p (X ( c (Y x))) => p ( b (Cl ( d x)))) =>
+			(forall p x . p (Dl (Y x)) => p (Y (D x))) =>
+			(forall p x . p (Dl (Dl (El x))) => p (Y (El x))) =>
+			(forall p x . p (B ( b (Cl ( d (D x))))) => p (Bl (Bl ( c (Dl (Dl x)))))) =>
+			p0 (Al (Bl (Bl ( c (Dl (Dl (El y)))))))
+f a1 a2 a3 a4 a5 a6 a7 a8 b = b (a6 (a5 (a7 (a4 (a8 (f a1 (\ c1 . a2 (a1 (a3 c1))) a3 a4 a5 (\ c1. a6 (a5 (a7 c1))) a7 a8 (\ c1 . a2 (a1 (a3 (a1 (a3 (b c1))))))))))))
