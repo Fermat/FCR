be82ce45728112d04575422087689bf020aaa509
Fermat <fermat1217@gmail.com>
2016-06-03 16:37:02 +0000

    found yet another typo in the paper

---
+++ tests/regII.rs
@@ -0,0 +1,14 @@
+K1 : Z (L x) ~> L (Z x)
+K2 : R (Z x) ~> Z (R x)
+K3 : Z (L (L x)) ~> Z (L (R x))
+K4 : R (R (Z x)) ~> L (Z (R (Z x)))
+
+f : forall p l r y . (forall p x . p (l (Z x)) => p (Z (l x))) =>
+                    (forall p x . p (Z (L (r x))) => p (Z (L (l x)))) =>
+                    (forall p x . p (Z (r x)) => p (r (Z x))) =>
+         	    (forall p x . p (l (Z (R (Z x)))) => p (r (R (Z x)))) => p (Z (L (l (Z (Z (R (Z y)))))))
+
+f a1 a3 a2 a4 = a3 (a2 (a2 (a4 (a1  (a1  (f a1 (\ c . a3 (a2 c)) a2 (\ c . a4 (a1 c)))))))) 
+
+h : (Z (L (L (Z (Z (R (Z y)))))))
+h = f K1 K3 K2 K4
